4e3305fdb63df439f0421d24145c9b45b06d93aa
Ataru Nakano <neu.nakano@gmail.com>
2019-04-16 20:27:32 +0900

    vue if02

--- www/js/vue.es.js
+++ www/js/vue.es.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#app',
     data: {
-      ok: false,
+      type: 'A',
     },
   })
 })
--- www/js/vue.js
+++ www/js/vue.js
@@ -1,1 +1,1 @@
-"use strict";document.addEventListener("DOMContentLoaded",function(){new Vue({el:"#app",data:{ok:!1}})});
+"use strict";document.addEventListener("DOMContentLoaded",function(){new Vue({el:"#app",data:{type:"A"}})});
