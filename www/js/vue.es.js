7aca31fedc94e1cce298b3028ed1dbd53b52103b
Ataru Nakano <neu.nakano@gmail.com>
2019-04-24 21:16:14 +0900

    vue $elと$refsが仮想DOMなことを確かめる

--- www/js/vue.es.js
+++ www/js/vue.es.js
@@ -4,34 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#app',
     data: {
-      list: [],
-    },
-    created() {
-      axios
-        .get('/js/list.json')
-        .then(response => {
-          this.list = response.data
-        })
-        .catch(e => {
-          console.error(e)
-        })
+      show: true,
     },
     methods: {
-      doAdd() {
-        var max = this.list.reduce(function(a, b) {
-          return a > b.id ? a : b.id
-        }, 0)
-        this.list.push({
-          id: max + 1,
-          name: this.name,
-          hp: 500,
-        })
-      },
-      doRemove(index) {
-        this.list.splice(index, 1)
-      },
-      doAttack(index) {
-        this.list[index].hp -= 10 // HPを減らす
+      handleClick() {
+        var count = this.$refs.count
+        if (count) {
+          count.innerText = parseInt(count.innerText) + 1
+        }
       },
     },
   })
