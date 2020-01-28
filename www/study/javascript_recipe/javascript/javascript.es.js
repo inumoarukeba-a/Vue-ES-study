171e46bdaa53dba322fc61118c7d673444758973
Ataru Nakano <atarunakano@gmail.com>
2020-01-28 20:13:43 +0900

    static method

--- www/study/javascript_recipe/javascript/javascript.es.js
+++ www/study/javascript_recipe/javascript/javascript.es.js
@@ -1,34 +1,9 @@
 'use strict'
 
 class MyClass {
-  consructor() {
-    this.result
-    this.error
-    this.name
-    this.age
+  static method() {
+    return 'hoge'
   }
 }
 
-const getApi = responce => {
-  const myClass = new MyClass()
-
-  myClass.result = responce.result
-  myClass.error = responce.error
-  myClass.name = responce.name
-  myClass.age = responce.age
-
-  console.log(`
-    result: ${myClass.result}
-    error: ${myClass.error}
-    name: ${myClass.name}
-    age: ${myClass.age}
-  `)
-}
-
-const responseData = {
-  result: true,
-  name: '中野',
-  age: 29,
-}
-
-getApi(responseData)
+console.log(MyClass.method())
