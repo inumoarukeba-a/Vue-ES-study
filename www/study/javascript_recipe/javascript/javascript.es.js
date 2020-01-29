3b71365a91346d7f88ee9dac49415f2f74b3cbdc
Ataru Nakano <atarunakano@gmail.com>
2020-01-29 20:51:03 +0900

    promise 直列処理

--- www/study/javascript_recipe/javascript/javascript.es.js
+++ www/study/javascript_recipe/javascript/javascript.es.js
@@ -1,25 +1,19 @@
 'use strict'
 
-const arrFunc = []
-for (let i = 0; i < 5; i++) {
-  const func = resolve => {
-    console.log(`処理${i}を開始`, new Date().toLocaleTimeString())
-    const delayMsec = 2000 * Math.random()
-
-    setTimeout(() => {
-      console.log(`処理${i}終了`, new Date().toLocaleTimeString())
-      resolve()
-    }, delayMsec)
-  }
-  arrFunc.push(func)
-}
-
-console.log(arrFunc)
-
-const arrPromise = arrFunc.map(func => new Promise(func))
-
-console.log(arrPromise)
-
-Promise.all(arrPromise).then(() => {
-  console.log('Finish')
-})
+Promise.resolve()
+  .then(() => {
+    new Promise(resolve => {
+      setTimeout(() => {
+        console.log('1', new Date().toLocaleDateString())
+        resolve()
+      }, 1000)
+    })
+  })
+  .then(() => {
+    new Promise(resolve => {
+      setTimeout(() => {
+        console.log('2', new Date().toLocaleDateString())
+        resolve()
+      }, 1000)
+    })
+  })
