0f9b397f9248a608f34a58b5327c1d8cbc61140a
Ataru Nakano <atarunakano@gmail.com>
2020-01-29 20:38:52 +0900

    promise all 並列処理

--- www/study/javascript_recipe/javascript/javascript.es.js
+++ www/study/javascript_recipe/javascript/javascript.es.js
@@ -1,16 +1,25 @@
 'use strict'
 
-const flag = false
-const promise = new Promise((resolve, reject) => {
-  if (flag === true) {
-    resolve('berry')
-  } else {
-    reject('mekabu')
+const arrFunc = []
+for (let i = 0; i < 5; i++) {
+  const func = resolve => {
+    console.log(`処理${i}を開始`, new Date().toLocaleTimeString())
+    const delayMsec = 2000 * Math.random()
+
+    setTimeout(() => {
+      console.log(`処理${i}終了`, new Date().toLocaleTimeString())
+      resolve()
+    }, delayMsec)
   }
+  arrFunc.push(func)
+}
+
+console.log(arrFunc)
+
+const arrPromise = arrFunc.map(func => new Promise(func))
+
+console.log(arrPromise)
+
+Promise.all(arrPromise).then(() => {
+  console.log('Finish')
 })
-  .then(value => {
-    console.log(value)
-  })
-  .catch(value => {
-    console.log(value)
-  })
